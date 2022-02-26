444ed869d9508897939933298d87de44ccd20196
Alexandr <Formak21@ya.ru>
2022-02-26 17:15:50 +0300

    upd

---
+++ decentralized/cclient.py
@@ -0,0 +1,17 @@
+import socket
+import json
+
+
+def f(n) -> int:
+    return sum(n)
+
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(('127.0.0.1', 12333))
+while True:
+    data = connection.recv(32768).decode('utf-8')
+    print(data)
+    data = json.loads(data)
+    data = f(data)
+    connection.send(f'{data}'.encode('utf-8'))
+    print(f'Data sent:{data}')
---
+++ decentralized/main2.py
@@ -0,0 +1,94 @@
+#
+#  main2.py
+#  GeneticAlgorithm main2
+#
+#  Created by Alexandr Formakovskiy ( Form49.d ).
+#
+import datetime
+import socket
+import json
+import sys
+
+sys.path.append('../lib')
+import GeneticIndividual
+import GeneticAlgorithm
+import ModernGraph
+
+VERSION = "4.0.3RePy_NET"
+
+if __name__ == '__main__':
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind(('127.0.0.1', 12333))
+    listener.listen(1)
+
+
+    def ConnectMachine(n):
+        print(f'Waiting for machine no{n}...')
+        machine = listener.accept()
+        print(f'Machine No{n} Successfully connected. ')
+        return machine
+
+
+    machines = int(input('how many machines:'))
+    Connected_Machines = [{'Machine': ConnectMachine(i), 'Status': 0, 'Data': -1} for i in range(machines)]
+
+
+    class NetworkGI(GeneticIndividual.GeneticIndividual):
+        def __init__(self, size):
+            super().__init(size)
+            self.quality_ind = None
+
+
+    class NetworkGA(GeneticAlgorithm.GeneticAlgorithm):
+        def quality_update(self):  # VERY SLOW FUNCTION, PLS DON'T CALL IT IF IT NO NEEDED
+            global Connected_Machines
+            queue = [i for i in range(self.SIZE)]
+            wait = []
+            machine_number = 0
+            while len(queue) or len(wait):
+                if Connected_Machines[machine_number]['Status'] == 1:
+                    self.individuals[Connected_Machines[machine_number]['Data']].quality_ind = int(
+                        Connected_Machines[machine_number]['Machine'][0].recv(32768).decode('utf-8'))
+                    wait.remove(Connected_Machines[machine_number]['Data'])
+                    Connected_Machines[machine_number]['Status'] = 0
+                    Connected_Machines[machine_number]['Data'] = -1
+                if Connected_Machines[machine_number]['Status'] == 0 and len(queue):
+                    Connected_Machines[machine_number]['Status'] = 1
+                    wait.append(queue.pop(0))
+                    Connected_Machines[machine_number]['Data'] = wait[-1]
+                    Data_for_send = json.dumps([int(i) for i in
+                                                self.individuals[
+                                                    Connected_Machines[machine_number]['Data']].individual])
+                    Connected_Machines[machine_number]['Machine'][0].send(Data_for_send.encode("utf-8"))
+                machine_number = (machine_number + 1) % machines
+
+    size = int(input('how many individuals:'))
+    leng = int(input('how many genes in one individual:'))
+    m_reg = input('mutation mode(WEAK/NORMAL/STRONG/NULL):')
+    Ga = NetworkGA(size, leng, m_reg, NetworkGI)
+    counter = 0
+    GaGraph = ModernGraph.ModernGraph(Ga, counter)
+    iters = int(input('how many iterations:'))
+    Started = datetime.datetime.now()
+    while True:
+        Ga.quality_update()
+        GaGraph.add_point()
+        # print(f"\rCycle:{counter}", end='')
+        # print(f"Max:{Test.max_quality()}")
+        # print(f"Max:{Test.min_quality()}")
+        # print(f"Population:\n[{Test}]\n")
+        if counter == iters:
+            print('Done!')
+            print(f'Started:{Started.strftime("%d.%m.%y-%H:%M:%S")}')
+            print(f'Ended:{datetime.datetime.now().strftime("%d.%m.%y-%H:%M:%S")}')
+            print(f'Delta seconds:{(datetime.datetime.now() - Started).seconds}')
+            print()
+            GaGraph.open_graph()
+            if not bool(int(input('Continue? 0/1:'))):
+                break
+            iters += int(input(f'now {counter} iterations left, how many more iterations:'))
+        Ga.crossover()
+        Ga.mutation()
+        Ga.selection()
+        counter += 1
