3170cd6c69a42f74a22917f147b16e1dc7e34066
Alexandr <Formak21@ya.ru>
2022-02-28 01:37:40 +0300

    upd

--- Test_Functions/TestFunction1.py
+++ Test_Functions/TestFunction1.py
@@ -1,12 +1,6 @@
 # y=x
-# x in [-10^9;10^9]
-
-# Params
-a = -(10**9)
-b = (10**9)
-
 
 # y = f(n); f(n) = x
 def f(n) -> float:
-    x = ((int(''.join([str(i) for i in n]), 2) / int('1' * len(n), 2)) * (b - a)) + a
+    x = int(''.join([str(i) for i in n]), 2)
     return x
--- Test_Functions/TestFunction2.py
+++ Test_Functions/TestFunction2.py
@@ -1,14 +1,16 @@
 # y=-((x1^2) + (x2^2))
-# x1 in [-20;20]
-# x2 in [-20;20]
+# x1 in [-5;5]
+# x2 in [-5;5]
 
 # Params
-a = -20
-b = 20
+a1 = -5
+b1 = 5
+a2 = -5
+b2 = 5
 
 
 # y = f(n); f(n) = -((x1^2) + (x2^2))
 def f(n) -> float:
-    x1 = ((int(''.join([str(i) for i in n[:len(n) // 2]]), 2) / int('1' * (len(n) // 2), 2)) * (b - a)) + a
-    x2 = ((int(''.join([str(i) for i in n[len(n) // 2:]]), 2) / int('1' * (len(n) // 2), 2)) * (b - a)) + a
+    x1 = ((int(''.join([str(i) for i in n[:len(n) // 2]]), 2) / int('1' * (len(n) // 2), 2)) * (b1 - a1)) + a1
+    x2 = ((int(''.join([str(i) for i in n[len(n) // 2:]]), 2) / int('1' * (len(n) // 2), 2)) * (b2 - a2)) + a2
     return -((x1 ** 2) + (x2 ** 2))
