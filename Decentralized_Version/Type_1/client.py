5752fe1f28a22346be2fd4d1b6f8648448cda9ef
Alexandr <Formak21@ya.ru>
2022-03-09 20:25:13 +0300

    upd

--- Decentralized_Version/Type_1/client.py
+++ Decentralized_Version/Type_1/client.py
@@ -15,14 +15,15 @@ from Test_Functions import TestFunction5
 from Test_Functions import TestFunction6
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(('127.0.0.1', 25566))
+connection.connect(('192.168.88.246', 25566))
 while True:
     data = connection.recv(65536).decode('utf-8')
     data = json.loads(data)
     GM = main.GeneticMain(Ga.GeneticAlgorithm, Gi.GeneticIndividual, Mg.EModernGraph, TestFunction6, data)
     GM.run(False)
-    Data1 = [[int(i) for i in ''.split(str(GM.solutions[0]))], GM.solutions[0].quality,
+    Data1 = [[int(i) for i in GM.solutions[0].individual], GM.solutions[0].quality,
              GM.deltas[0].microseconds / 1000, GM.es[0]]
     Data2 = GM.grs[0].points
     connection.send(json.dumps(Data1).encode('utf-8'))
+    connection.recv(512)
     connection.send(json.dumps(Data2).encode('utf-8'))
