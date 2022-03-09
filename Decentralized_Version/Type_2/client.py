5dca4f429c410b582cda42a09ebe5a7cb2b7c0ae
Alexandr <Formak21@ya.ru>
2022-03-10 02:36:51 +0300

    Type_2

---
+++ Decentralized_Version/Type_2/client.py
@@ -0,0 +1,25 @@
+import socket
+import json
+
+from Library import GeneticIndividual as Gi
+from Library import GeneticAlgorithm as Ga
+from Library import EModernGraph as Mg
+from Library import main
+
+from Test_Functions import TestFunction0
+from Test_Functions import TestFunction1
+from Test_Functions import TestFunction2
+from Test_Functions import TestFunction3
+from Test_Functions import TestFunction4
+from Test_Functions import TestFunction5
+from Test_Functions import TestFunction6
+
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect(('192.168.88.246', 25566))
+while True:
+    data = connection.recv(65536).decode('utf-8')
+    data = json.loads(data)
+    GM = main.GeneticMain(Ga.GeneticAlgorithm, Gi.GeneticIndividual, Mg.EModernGraph, TestFunction6, data)
+    GM.run(False)
+    Data1 = [[int(i) for i in GM.solutions[0].individual], GM.solutions[0].quality]
+    connection.send(json.dumps(Data1).encode('utf-8'))
