3969665c56133597e1b8f96ad8f36e443750faa5
Alexandr <Formak21@ya.ru>
2022-03-10 15:23:16 +0300

    upd

--- Decentralized_Version/Type_3/client.py
+++ Decentralized_Version/Type_3/client.py
@@ -1,10 +1,9 @@
+import random
 import socket
 import json
 
 from Library import GeneticIndividual as Gi
 from Library import GeneticAlgorithm as Ga
-from Library import EModernGraph as Mg
-from Library import main
 
 from Test_Functions import TestFunction0
 from Test_Functions import TestFunction1
@@ -19,7 +18,27 @@ connection.connect(('192.168.88.246', 25566))
 while True:
     data = connection.recv(65536).decode('utf-8')
     data = json.loads(data)
-    GM = main.GeneticMain(Ga.GeneticAlgorithm, Gi.GeneticIndividual, Mg.EModernGraph, TestFunction4, data)
-    GM.run(False)
-    Data1 = [[int(i) for i in GM.solutions[0].individual], GM.solutions[0].quality]
-    connection.send(json.dumps(Data1).encode('utf-8'))
+    ga = Ga.GeneticAlgorithm(data[0], data[1], data[2], Gi.GeneticIndividual, TestFunction4.f)
+    population = 0
+    while True:
+        ga.quality_update()
+        ga.update_best_solution()
+        if random.choice([True, False]):
+            connection.send(
+                json.dumps([[int(i) for i in ga.individuals[ga.best_per_population()].individual],
+                            ga.individuals[ga.best_per_population()].quality]).encode('utf-8'))
+            data_tmp = json.loads(connection.recv(65536).decode('utf-8'))
+            data_tmp[0] = Gi.GeneticIndividual(data[1], data_tmp[0])
+            data_tmp[0].quality = data_tmp[1]
+            ga.individuals[ga.best_per_population()] = data_tmp[0]
+            ga.update_best_solution()
+        else:
+            connection.send(json.dumps([0, 0]).encode('utf-8'))
+        if population > data[3]:
+            connection.send(
+                json.dumps([[int(i) for i in ga.best_solution.individual], ga.best_solution.quality]).encode('utf-8'))
+            break
+        ga.selection()
+        ga.crossover()
+        ga.mutation()
+        population += 1
--- Decentralized_Version/Type_3/server.py
+++ Decentralized_Version/Type_3/server.py
@@ -8,6 +8,7 @@ import datetime
 import socket
 import json
 import math
+import random
 
 from Library import GeneticIndividual as Gi
 from Test_Functions import TestFunction4
@@ -29,7 +30,7 @@ if __name__ == '__main__':
 
 
     machines = int(input('how many machines:'))
-    Connected_Machines = [{'Machine': connect_machine(i), 'Status': 0, 'Data': -1} for i in range(machines)]
+    Connected_Machines = [connect_machine(i) for i in range(machines)]
 
     individuals_quantity = int(input('how many individuals(N%2=0):'))
     gene_quantity = int(input('how many genes in one individual:'))
@@ -42,27 +43,26 @@ if __name__ == '__main__':
     def best_solution_finder(x):
         return max(x, key=lambda y: y.quality)
 
-
-    for _ in range(12):
-        queue = [i for i in range(machines)]
-        wait = []
-        machine_number = 0
-        while len(queue) or len(wait):
-            if Connected_Machines[machine_number]['Status'] == 1:
-                data = json.loads(Connected_Machines[machine_number]['Machine'][0].recv(65536).decode('utf-8'))
-                solutions[-1].append(Gi.GeneticIndividual(gene_quantity, data[0]))
-                solutions[-1][-1].quality = data[1]
-                wait.remove(Connected_Machines[machine_number]['Data'])
-                Connected_Machines[machine_number]['Status'] = 0
-                Connected_Machines[machine_number]['Data'] = -1
-            if Connected_Machines[machine_number]['Status'] == 0 and len(queue):
-                Connected_Machines[machine_number]['Status'] = 1
-                wait.append(queue.pop(0))
-                Connected_Machines[machine_number]['Data'] = wait[-1]
-                data_for_send = json.dumps(
-                    [individuals_quantity, gene_quantity, mutation_mode, population_quantity])
-                Connected_Machines[machine_number]['Machine'][0].send(data_for_send.encode("utf-8"))
-            machine_number = (machine_number + 1) % machines
+    for machine in Connected_Machines:
+        data_for_send = json.dumps([individuals_quantity, gene_quantity, mutation_mode, population_quantity])
+        machine[0].send(data_for_send.encode("utf-8"))
+    for _ in range(population_quantity):
+        individual_bank = []
+        machine_bank = []
+        for machine in Connected_Machines:
+            data = json.loads(machine[0].recv(65536).decode('utf-8'))
+            if data[0] is int:
+                continue
+            else:
+                individual_bank.append(data)
+                machine_bank.append(machine)
+        for machine in machine_bank:
+            data_for_send = individual_bank.pop(random.randint(0, len(individual_bank)-1))
+            machine[0].send(data_for_send.encode("utf-8"))
+    for machine in Connected_Machines:
+        data = json.loads(machine[0].recv(65536).decode('utf-8'))
+        solutions[-1].append(Gi.GeneticIndividual(gene_quantity, data[0]))
+        solutions[-1][-1].quality = data[1]
         solutions[-1] = best_solution_finder(solutions[-1])
         solutions.append([])
 
